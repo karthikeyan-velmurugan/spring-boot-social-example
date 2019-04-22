f3d107dee7aec1e760020f2bdc779eeb8331898a
Karthikeyan Velmurugan <karthikvel90@gmail.com>
2019-04-22 12:36:31 +0530

    Facebook, Google and Github Properties added.

--- src/main/java/com/social/controller/FacebookController.java
+++ src/main/java/com/social/controller/FacebookController.java
@@ -3,7 +3,11 @@
  */
 package com.social.controller;
 
+import java.util.Set;
+
+import org.springframework.social.facebook.api.Facebook;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,10 +18,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/login")
 public class FacebookController {
-	
+
+	private Facebook facebook;
+
+	public FacebookController(Facebook facebook) {
+		this.facebook=facebook;
+	}
+
 	@GetMapping
 	public String welcome() {
 		return "Welcome to facebook controller";
 	}
 
+	//@GetMapping("/fblogin/{username}/{password}")
+	@GetMapping("/fblogin")
+	public Set<String> getFbProfile(@PathVariable String username, @PathVariable String password ){
+
+		org.springframework.social.facebook.api.User userProfile = facebook.userOperations().getUserProfile();
+		userProfile.getExtraData();
+
+		return userProfile.getExtraData().keySet();
+
+	}
 }
